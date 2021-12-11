5f1fef83c06a9fd3c77e3acaec7bed689e941d3a
chaimaejiji1234 <95656579+chaimaejiji1234@users.noreply.github.com>
2021-12-11 22:24:17 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,35 +1,122 @@
-const description =
-  "This is the description of your NFT project, remember to replace this";
-const baseUri = "https://hashlips/nft";
+const basePath = process.cwd();
+const { MODE } = require(`${basePath}/constants/blend_mode.js`);
+const { NETWORK } = require(`${basePath}/constants/network.js`);
 
-const layersOrder = [
-  { name: "Background", number:4 },
-  { name: "Base" , number :1},
-  { name: "cash", number :4 },
-  { name: "Chapeau", number:4 },
-  { name: "hands", number: 3 },
+const network = NETWORK.eth;
+
+// General metadata for Ethereum
+const namePrefix = "Your Collection";
+const description = "Remember to replace this description";
+const baseUri = "ipfs://NewUriToReplace";
+
+const solanaMetadata = {
+  symbol: "YC",
+  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
+  external_url: "https://www.youtube.com/c/hashlipsnft",
+  creators: [
+    {
+      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
+      share: 100,
+    },
+  ],
+};
+
+// If you have selected Solana then the collection starts from 0 automatically
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 5,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Eyeball" },
+      { name: "Eye color" },
+      { name: "Iris" },
+      { name: "Shine" },
+      { name: "Bottom lid" },
+      { name: "Top lid" },
+    ],
+  },
 ];
 
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
 const format = {
-  width: 1280,
-  height: 1280,
+  width: 512,
+  height: 512,
+  smoothing: false,
+};
+
+const gif = {
+  export: false,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
 };
 
 const background = {
   generate: true,
   brightness: "80%",
+  static: false,
+  default: "#000000",
 };
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
 
 const uniqueDnaTorrance = 10000;
 
-const editionSize = 10;
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 5,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
 
 module.exports = {
-  layersOrder,
   format,
-  editionSize,
   baseUri,
   description,
   background,
   uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  solanaMetadata,
+  gif,
+  preview_gif,
 };
